8c8a4281bce49425becffc5816188b14b639268b
Muhammad Hadiansyah <hadiansyah21rabbani5@gmail.com>
2023-09-25 08:15:10 +0700

    First Commit

---
+++ io.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  print('Latihan Input-Output');
+
+  print('Enter first number: ');
+  int? n1 = int.parse(stdin.readLineSync()!);
+
+  print('Enter second number: ');
+  int? n2 = int.parse(stdin.readLineSync()!);
+
+  int sum = n1 + n2;
+  print('Sum is $sum');
+}
