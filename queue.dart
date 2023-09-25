4eab0e6dc1d8c5bcb31abeb538c9658cdcd4570a
Muhammad Hadiansyah <hadiansyah21rabbani5@gmail.com>
2023-09-25 08:53:02 +0700

    Adding queue.dart files

---
+++ queue.dart
@@ -0,0 +1,13 @@
+import 'dart:collection';
+
+void main() {
+  Queue<String> sis = new Queue<String>();
+
+  print(sis);
+
+  sis.add('Sistem');
+  sis.add('Informasi');
+  sis.add('Seluler');
+
+  print(sis);
+}
